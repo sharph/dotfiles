a556eee9a7d2bac20fce8fd6475fe0920d36a0bd
Sharp Hall <sharp@sharphall.org>
2025-07-05 11:27:05 -0400

    configure anthropic in codecompanion

--- nvim/.config/nvim/lua/custom/plugins/codecompanion.lua
+++ nvim/.config/nvim/lua/custom/plugins/codecompanion.lua
@@ -1,7 +1,30 @@
 return {
   'olimorris/codecompanion.nvim',
   config = function()
-    require('codecompanion').setup()
+    require('codecompanion').setup {
+      adapters = {
+        anthropic = function()
+          return require('codecompanion.adapters').extend('anthropic', {
+            env = {
+              api_key = 'cmd:op read op://personal/anthropic/credential --no-newline',
+            },
+          })
+        end,
+      },
+      strategies = {
+        chat = {
+          adapter = 'anthropic',
+          --          keymaps = {
+          --            next_chat = {
+          --              modes = { n = 'g}' },
+          --            },
+          --            previous_chat = {
+          --              modes = { n = 'g{' },
+          --            },
+          --          },
+        },
+      },
+    }
   end,
   requires = {
     'nvim-lua/plenary.nvim',
