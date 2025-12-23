d3a2f3babf048eae4cf1287a1bb05145074243e9
Sharp Hall <sharp@sharphall.org>
2025-12-23 18:15:49 -0500

    re-import fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,22 +1,23 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    atuin init fish | source
 end
 
 set -g fish_greeting
 
+fish_config theme choose TokyoNight\ Night
+
 starship init fish | source
 
-
-# uv
-fish_add_path "~/.local/bin"
+fish_add_path "$HOME/.local/bin"
+fish_add_path "$HOME/.cargo/bin"
 
 fish_add_path "/usr/lib/cargo/bin/coreutils"
 
-# alias ssh="kitty +kitten ssh"
 alias ls="eza --git --icons=auto"
 alias cat=bat
 
 # Added by Radicle.
-export PATH="$PATH:~/.radicle/bin"
+export PATH="$PATH:$HOME/.radicle/bin"
 
 zoxide init fish | source
