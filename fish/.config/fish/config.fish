adde7c05e58b0d78f31e55bc2036a75cbdf164e3
Sharp Hall <sharp@sharphall.org>
2025-12-22 21:33:45 -0500

    install some new cli command replacements

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -13,7 +13,10 @@ fish_add_path "/home/sharp/.local/bin"
 fish_add_path "/usr/lib/cargo/bin/coreutils"
 
 # alias ssh="kitty +kitten ssh"
-alias ls=lsd
+alias ls="eza --git --icons=auto"
+alias cat=bat
 
 # Added by Radicle.
 export PATH="$PATH:/home/sharp/.radicle/bin"
+
+zoxide init fish | source
